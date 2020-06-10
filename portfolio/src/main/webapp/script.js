b77b3afc30391546ae2314440efdf411937bd63b
Asha <ajohnson@sps-program.com>
2020-06-10 10:57:27 -0700

    Completed Week 2: Step 2 Fetch Walkthrough

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
+
 function addRandomFact() {
   const funfacts =
       ["I have a orange, long hair Maine Coon cat mix named Kitty Kitty", "I'm 6'1\"", "I can make origami cranes without looking at the paper", "I'm still in denial of my fear of heights", "I make excellent baklava", "I don't know how to ride a bike", "My highschool volleyball team won the CA State Championships", "I have 5 wisdom teeth"];
@@ -27,4 +25,8 @@ function addRandomFact() {
   funfactContainer.innerText = funfact;
 }
 
-
+function getGreetingUsingArrowFunctions() {
+  fetch('/data').then(response => response.text()).then((greeting) => {
+    document.getElementById('greeting-container').innerText = greeting;
+  });
+}
